be1b2b6cd23900cda3b0d94711d8fd5747eea660
maxempoint <mep@mep.localdomain>
2022-08-04 09:04:51 +0200

    Changed to new function call

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -2,6 +2,7 @@ import unittest
 import sys
 from io import StringIO
 import logging
+import json
 
 sys.path.append('../')
 import tk_gui
@@ -13,7 +14,7 @@ class TestGUI(unittest.TestCase):
 
     def setUp(self):
         logging.basicConfig(level=logging.WARNING)
-        MOCK_DATA_FILENAME = "test_data.dat"#"test_all_games.dat"#"## #
+        MOCK_DATA_FILENAME = "test_data.dat"#"test_all_games.dat"#
         EXPORT_FILENAME = "test_export_file_for_gamecheatdata_class.dat"
 
         self.model = Model(EXPORT_FILENAME, MOCK_DATA_FILENAME, mock=True)
@@ -24,9 +25,6 @@ class TestGUI(unittest.TestCase):
     
     def mock_callback(self, userinput: UserInput):
         self.callback_ret = userinput.get_action_and_data()
-
-    def stringify(self, string):
-        return string.rstrip().decode()
 
     #test if all ctrl messages are handled
     def test01_user_actions_present_data(self):
@@ -39,10 +37,9 @@ class TestGUI(unittest.TestCase):
             addresses = cheat.get_sanitized_cheatCodeAddresses()
 
             self.view.select_game_menu.set(game_name)
-            gui_data = self.view.get_user_action(UserAction.SHOW_ALL_DATA_FROM_AR)
-
+            self.view.prepare_and_exec_callback(UserAction.SHOW_ALL_DATA_FROM_AR)
             #Check data if UserInput data in callback_ret is correct
-            self.assertEqual(game_name, gui_data[0]['game_name'])
+            self.assertEqual(game_name, self.callback_ret)
 
     
     def test02_correct_data_is_presented(self):
@@ -55,11 +52,11 @@ class TestGUI(unittest.TestCase):
             addresses = cheat.get_sanitized_cheatCodeAddresses()
 
             self.view.select_game_menu.set(game_name)
-            gui_data = self.view.get_user_action(UserAction.SHOW_ALL_DATA_FROM_AR)
+            gui_data = self.view.prepare_and_exec_callback(UserAction.SHOW_ALL_DATA_FROM_AR)
 
-            #check if number of cheatcodes presented in GUI is the same as in the model:
-            #TODO check on the option menu now...
-            #self.assertEqual(len(gui_data), num_cheatcodes)
+            #check if number of cheatcodes presented in GUI is the same as in the model
+            #the number of cheatcodes equals the number of entries in the cheatcode option menu
+            self.assertEqual(len(gui_data), len( self.view.cheatcode_opt_menu.winfo_children() ))
 
             #check if correct data is presented after fetch_model_data() (via humble object pattern)
             for insert in gui_data:
@@ -75,18 +72,17 @@ class TestGUI(unittest.TestCase):
     def test03_modify_data(self):
         gameCheats = self.model.game_cheats
         gui_data = []
-        test_game_name = "test"
+        test_game_name = list(json.loads( self.model.get_games_as_json() ).items())[0][0]
 
         self.view.clear_gui()
 
         #>GUI gets data
+        #print(list(json.loads( self.model.get_games_as_json() ).items())[0][0])
         #Insert mutltiple cheatcodes+addresses with the same game name
         gui_data.append(self.view.insert_into_gui(test_game_name, "my_cheat_code00", ["0x0","0x1","0x2"]))
-        gui_data.append(self.view.insert_into_gui(test_game_name, "my_cheat_code01", ["0x3","0x4","0x5"]))
-        gui_data.append(self.view.insert_into_gui(test_game_name, "my_cheat_code02", ["0x4","0x7","0x8"]))
 
         #Trigger the modify data action
-        self.view.get_user_action(UserAction.MODIFY_DATA)
+        self.view.prepare_and_exec_callback(UserAction.MODIFY_DATA)
 
         #extract the data that was given to the insert_function
         gui_cheatcodes = []
@@ -97,12 +93,14 @@ class TestGUI(unittest.TestCase):
             gui_addresses.append(d["addresses"])
 
         #<GUI returns data in the form {String : {String : [HexStrings]}}
+        print(self.callback_ret["data"])
         callback_game_name = list( self.callback_ret["data"] )[0]
         callback_cheatcodes = list(self.callback_ret["data"][callback_game_name].keys())
         callback_addresses = list(self.callback_ret["data"][callback_game_name].values())
 
         #Check type
-        self.assertEqual(type( {"test" : {"c0" : ["1","2"]}} ),type( self.callback_ret["data"][callback_game_name] ))
+        self.assertEqual(type( {"test" : {"c0" : ["1","2"]}} ),
+                        type( self.callback_ret["data"][callback_game_name] ))
 
         self.assertEqual(gui_game_name, callback_game_name)
         self.assertEqual(gui_cheatcodes, callback_cheatcodes)
